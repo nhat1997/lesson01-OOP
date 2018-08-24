f63277ea3881712ef138ad6d9df3322708f7c829
Nhat <nmnhat1602@gmail.com>
2018-08-24 15:44:01 +0700

    finish Person

---
+++ src/nhat/dev/MainApp.java
@@ -0,0 +1,24 @@
+package nhat.dev;
+
+import nhat.dev.models.Person;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final String arrName[] = {"Bao", "Tu", "Tap", "Teo", "Nam"};
+		final int arrOld[] = {21, 25, 26, 15, 16};
+		
+		Person arrPerson[] = new Person[arrName.length];
+		for(int i=0; i < arrPerson.length; i++) {
+			Person p = new Person(arrName[i], arrOld[i]);
+			
+			arrPerson[i] = p;
+		}
+		for(int i=0; i < arrPerson.length; i++) {
+		arrPerson[i].show();
+		}
+
+	}
+
+}
